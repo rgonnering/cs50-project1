46615801e437658bc596622148f26fe2ee061dd7
Roger Gonnering <roger.gonnering@gmail.com>
2018-12-08 21:59:34 -0600

    Functionality Working

--- application.py
+++ application.py
@@ -42,22 +42,27 @@ notes = {}  				# book notes dictionary
 
 @app.route("/")
 def index():
+    if 'username' in session:
+        session.pop('username')
+    if 'bood_id' in session:
+        session.pop('book_id')
     return render_template('index.html')
 
 
 @app.route('/signup', methods = ['POST'])
 def signup():
     email = request.form.get('email')
-    usern = request.form.get('name')
-    passw = request.form.get('password')
-    email = request.form.get('email')
+    usern = request.f500pxorm.get('name')
+    pass500500px = request.form.get('password')
+    e500500pxxmail = request.form.get('email')
     # Check is user is already registered
-    #userSelection = db.execute("SELECT (username,password,email) FROM registered where username = :user", {"user": usern}).fetchone() 
+    #userSelection = db.execute("SELECT (username,password,email) FROM registered             <li><a href="{{ url_for('menu') }}">Search Again</a></li>where username = :user", {"user": usern}).fetchone() 
     result = db.execute("SELECT * FROM registered WHERE username = :u", {"u": usern}).fetchone()
-    print("Registration: ", usern, result[1], result.username )
-    if result.username == usern:
-        msg = "You are already Registered"
-        return render_template('form.html', email=email, username=usern, password=passw, msg=msg)
+
+    if result:
+        if result.username == usern:
+            msg = "You are already Registered"
+            return render_template('form.html', email=email, username=usern, password=passw, msg=msg)
     # New Registration
     result = db.execute("INSERT INTO registered (username, password, email) VALUES (:u, :p, :e)", {"u": usern, "p": passw, "e":email})
     db.commit()
@@ -67,46 +72,51 @@ def signup():
 
 @app.route('/login', methods=['POST'])
 def login():
-    # check if session['username' exists]
-    if session.get('username') is None:
-        session['username'] = []
-    print ("login: session: ", session['username'])
+
     # # get login credentials
     usern = request.form.get('name')
     passw = request.form.get('password')
+    print ("login1: ", usern, passw)   
 
     # Check if username is already logged in
-    if 'usern' in session:
+    if 'username' in session:
         msg = "You are logged in."
         return render_template('search.html', username=usern, msg=msg)
 
     # User is not logged in, so check if user matches a Registered user
     result = db.execute("SELECT * FROM registered where username = :u", {"u": usern}).fetchone()    
-    print("Login: ", usern, result[1], result.username )
+    if result:
+        print("Login2: ", usern, result[1], result.username )
 
-    if result.username == usern:
-        if result.password == passw:
+        if result.username == usern and result.password == passw:
             # login credentials match a registered user
             #session['username'].append(usern) 
             session['username'] = usern
-            if usern in session:
-                print ("Login session variable: ", usern, session['username'])
+            if 'username' in session:
+                print ("Login2 session variable: ", usern, session['username'])
             msg = "You have successfully Logged In!."
             return render_template('search.html', username=usern, msg=msg)
+        else: 
+            error_msg = "Username and Password do not match"
+            return render_template('login.html', msg=error_msg)            
     else:
-        error_msg = "Please Register"
-        return render_template('form.html', msg=error_msg)   
+        error_msg = "Invalid Credentials. Please Try Again, or Register"
+        return render_template('login.html', msg=error_msg)   
 
 
-@app.route('/menu', methods=['POST'])
+@app.route('/menu', methods=['GET', 'POST'])
 def menu():
+    
     action = request.form.get('action')
     if action == "register":
         return render_template('signup.html')    
     elif action == "login":
         return render_template('login.html')
     elif action == "search":
-        return render_template('search.html')
+        if 'username' in session:
+             return render_template('search.html')
+        else: 
+            return render_template('login.html')
     elif action == "goodread":
         # get Goodread 
         myISBN = "1632168146"
@@ -115,32 +125,36 @@ def menu():
 
         return render_template('goodread.html', res=res)    
     else:
-         return render_template('index.html', msg="Error, Unknown Action Requested")
+        return render_template('index.html')
 
 
 @app.route('/search', methods=['POST'])
 def search():
-    username = session['username']
-    print ("Search: ", username)
+
+
     isbn = request.form.get("isbn")
     title = request.form.get("title")
     author = request.form.get("author")
     year = request.form.get("year")
-    username = request.form.get("username")
 
     # execute SQL command and get all data with user constraintes 
-    count = 10
     selections = db.execute("SELECT * FROM books WHERE title LIKE '%"+title+"%' and author LIKE '%"+author+"%' and year LIKE '%"+year+"%' ").fetchall()
+    
+    # check session variables
+    username = session['username']
+
+    print("Search2: ", session['username'])
     return render_template('list_selections.html', selections=selections, username=username)
 
 
 @app.route('/book', methods=['POST'])
 def book():
     username = session['username']
-    print("Book - username: ", username)
 
     # get book data for id 
     id = request.form.get("select")  
+    print("Book1 ", username, id)  
+
     if id:
         session['book_id'] = id
     else:
@@ -153,7 +167,7 @@ def book():
 
     # get Goodread Rating
     myISBN = selection[1]
-    myKey = "KyNzMuBWuCAOoi42uosr8A"
+    myKey = "KyNzMuBWuCAOoi42uosr8A"# logout -------------------------------------------------------
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": myKey, "isbns": myISBN})
     response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": myKey, "isbns": myISBN})
     data = response.json()
@@ -180,29 +194,25 @@ def book():
     return render_template('book.html', username=username, selection=selection, notes=bookNotes, rating=gr_rating)
 
 
+# goSearch -------------------------------------------------------
+# from book.html, do another search
+@app.route('/goSearch', methods=['GET', 'POST'])
+def goSearch():
+    if 'username' in session:
+            return render_template('search.html')
+    else: 
+        return render_template('login.html')
+
 
 # logout -------------------------------------------------------
 @app.route("/logout")
 def logout():
     session.pop('username', None)
-    return redirect(url_for('login'))
+    return render_template('login.html')
 
 
 
 
-
-
-
-
-# not needed
-@app.route('/goodread', methods=['POST'])
-def goodread():
-    myISBN = "0316113573"
-    myKey = "KyNzMuBWuCAOoi42uosr8A"
-    response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": myKey, "isbns": myISBN})
-    data = response.json()
-    gr_rating = (data['books'][0]['average_rating'])
-
-    return render_template('goodread.html', response=response, rating=gr_rating)
+# main -------------------------------------------------------
 if __name__ == "__main__":
     app.run()
