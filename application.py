8b059fd3957dfb7162cff88d693b3388ac42fec6
Roger Gonnering <roger.gonnering@gmail.com>
2018-12-10 21:22:25 -0600

    fixed some css problems

--- application.py
+++ application.py
@@ -4,7 +4,7 @@
 
 # import packages
 import os
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from flask import Flask, session
 from flask_session import Session
 import requests                         # needed for Goodread API
@@ -29,36 +29,20 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-# declare global empty lists for users for this Session
-
-signedUpUsers = []			# list of registered users
-logedInUsers = []			# list of logged-in users
-username = ""				# session username
-notes = {}  				# book notes dictionary
-							# key : value
-							# where value is a list of notes
-							# isbn: notes[]
-
 
 @app.route("/")
 def index():
-    if 'username' in session:
-        session.pop('username')
-    if 'bood_id' in session:
-        session.pop('book_id')
     return render_template('index.html')
 
 
 @app.route('/signup', methods = ['POST'])
 def signup():
     email = request.form.get('email')
-    usern = request.f500pxorm.get('name')
-    pass500500px = request.form.get('password')
-    e500500pxxmail = request.form.get('email')
+    usern = request.form.get('name')
+    passw = request.form.get('password')
+    email = request.form.get('email')
     # Check is user is already registered
-    #userSelection = db.execute("SELECT (username,password,email) FROM registered             <li><a href="{{ url_for('menu') }}">Search Again</a></li>where username = :user", {"user": usern}).fetchone() 
     result = db.execute("SELECT * FROM registered WHERE username = :u", {"u": usern}).fetchone()
-
     if result:
         if result.username == usern:
             msg = "You are already Registered"
@@ -77,17 +61,14 @@ def login():
     usern = request.form.get('name')
     passw = request.form.get('password')
     print ("login1: ", usern, passw)   
-
     # Check if username is already logged in
     if 'username' in session:
         msg = "You are logged in."
         return render_template('search.html', username=usern, msg=msg)
-
     # User is not logged in, so check if user matches a Registered user
     result = db.execute("SELECT * FROM registered where username = :u", {"u": usern}).fetchone()    
     if result:
         print("Login2: ", usern, result[1], result.username )
-
         if result.username == usern and result.password == passw:
             # login credentials match a registered user
             #session['username'].append(usern) 
@@ -106,8 +87,8 @@ def login():
 
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
-    
     action = request.form.get('action')
+    print("Menu - action: ", action)
     if action == "register":
         return render_template('signup.html')    
     elif action == "login":
@@ -122,10 +103,10 @@ def menu():
         myISBN = "1632168146"
         myKey = "KyNzMuBWuCAOoi42uosr8A"
         res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": myKey, "isbn": myISBN} )
-
         return render_template('goodread.html', res=res)    
-    else:
-        return render_template('index.html')
+    elif action=="logout":
+        print("Menu: logout")
+        return redirect(url_for('logout'))
 
 
 @app.route('/search', methods=['POST'])
@@ -136,7 +117,6 @@ def search():
     author = request.form.get("author")
     year = request.form.get("year")
     print("Search2: ", session['username'])
-
     # execute SQL command and get all data with user constraintes 
     selections = db.execute("SELECT * FROM books WHERE title LIKE '%"+title+"%' and author LIKE '%"+author+"%' and year LIKE '%"+year+"%' ").fetchall()
     # check if book is in the books database
@@ -149,11 +129,9 @@ def search():
 @app.route('/book', methods=['POST'])
 def book():
     username = session['username']
-
     # get book data for id 
     id = request.form.get("select")  
     print("Book1 ", username, id)  
-
     if id:
         session['book_id'] = id
     else:
@@ -161,10 +139,8 @@ def book():
     comment = request.form.get("notefield")
     rating = request.form.get("action")
     print("Book Comment: ", username, id, comment, rating)
-
     # get books data for id
     selection = db.execute("SELECT * FROM books where id = :id", {"id": id}).fetchone()
-
     # get Goodread Rating
     myISBN = selection[1]
     myKey = "KyNzMuBWuCAOoi42uosr8A"# logout -------------------------------------------------------
@@ -172,25 +148,20 @@ def book():
     response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": myKey, "isbns": myISBN})
     data = response.json()
     gr_rating = (data['books'][0]['average_rating'])
-
     print("Book Content: ", id, username, selection.isbn, comment, rating)
-
     # Add notes, in applicable
     if not rating:
         rating = 3
-
     if comment:
         result = db.execute("INSERT INTO bookcomments (username,isbn,mycomment,myreview) \
             VALUES (:u, :isbn, :c, :r)", \
             { "u": username, "isbn": selection.isbn, "c": comment, "r": rating })
         db.commit()    
 
-
     # get books data for id
     selection = db.execute("SELECT * FROM books where id = :id", {"id": id}).fetchone()
     # get notes, if avaiable
     bookNotes = db.execute("SELECT * FROM bookcomments where isbn = :isbn", {"isbn": selection.isbn}).fetchall()
-
     # render book.html
     print("Book Render1: ", username, selection)
     print("Book Render2: ", bookNotes, rating)    
@@ -210,10 +181,13 @@ def goSearch():
 # logout -------------------------------------------------------
 @app.route("/logout")
 def logout():
-    session.pop('username', None)
+    print("logout")
+    #session.pop('username', None)
+    if 'username' in session:
+        session.pop('username')
+    if 'bood_id' in session:
+        session.pop('book_id')
     return render_template('login.html')
-
-
 
 
 # main -------------------------------------------------------
